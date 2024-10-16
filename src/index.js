41130e19730c7bbb1d1ef7a4218b172cd4249c61
sparky8512 <76499194+sparky8512@users.noreply.github.com>
2024-10-16 13:25:39 -0700

    Clean out no longer relevant items
    Remove references to websites that are no longer in operation, as well as any scripts that were specific to them. This greatly reduces the set of scripts that need to be adjusted to make work with later browser versions, and reduces permissions cruft in the manifest. There's probably a lot of CDN hosts that can be removed, too, but that can wait until run-time permissions is implemented.

--- src/index.js
+++ src/index.js
@@ -2,55 +2,29 @@ var host = window.location.host;
 
 if(document.querySelector("span#azerty") === null){
     // to make code run only once 
-    if(host !== "www.qiman6.com" && host !== "qingman5.com" && host !== "www.mangapanda.com"){
-        var mark = document.createElement("span");
-        mark.id = "azerty";
-        mark.style.display = "none";
+    var mark = document.createElement("span");
+    mark.id = "azerty";
+    mark.style.display = "none";
+    document.body.appendChild(mark);
 
-        document.body.appendChild(mark);
-        // tell the browser to use HTTPS rather than HTTP.
-        let upgradeInsecure = document.createElement("meta");
-        upgradeInsecure.httpEquiv = "Content-Security-Policy";
-        upgradeInsecure.content = "upgrade-insecure-requests";
-        document.head.insertBefore(upgradeInsecure,document.head.firstElementChild);
-    }
-    
+    // tell the browser to use HTTPS rather than HTTP.
+    let upgradeInsecure = document.createElement("meta");
+    upgradeInsecure.httpEquiv = "Content-Security-Policy";
+    upgradeInsecure.content = "upgrade-insecure-requests";
+    document.head.insertBefore(upgradeInsecure,document.head.firstElementChild);
+
     switch (host) {
-        case "www.mangareader.net":
-            mangareaderF();
-            break;
-        case "www.mangapanda.com":
-            mangapandaF();
-            break;
         case "mangakiss.org":
         case "mangakomi.com":
-        case "mangazone.cc":
-        case "mangafort.com":
-        case "1stkissmanga.com":
-        case "1stkissmanga.io":
-        case "zinmanga.com":
-        case "365manga.com":
-        case "toonily.net":
         case "toonily.com":
-        case "mangatx.com":
         case "mangatx.to":
-        case "aloalivn.com":
         case "manhuaplus.com":
-        case "mangaclash.com":
         case "toonclash.com":
-        case "manhuasworld.com":
-            mangakissF();   // is the same as mangakiss.org
+            mangakissF();
             break;
         case "manhuaus.com":
-            manhuausF();    // is the same as mangakiss.org
+            manhuausF();
             break;
-        case "ww3.mangafox.online":
-        case "ww4.mangafox.online":
-            mangafoxF();
-            break;
-        case "kissmanga.com":
-            kissmangaF();
-            break;        
         case "mangakakalot.com":
             mangakakalotF();
             break;
@@ -58,13 +32,11 @@ if(document.querySelector("span#azerty") === null){
         case "chapmanganelo.com":
         case "manganato.com":
         case "chapmanganato.to":
-        case "readmanganato.com":
             manganeloF();
             break;
         case "ww5.readhaikyuu.com":
         case "ww7.readsnk.com":
         case "ww3.readbleachmanga.com":
-        case "ww2.readnoblesse.com":
             readnarutoF();
             break;
         case "ww5.readblackclover.com":
@@ -98,73 +70,36 @@ if(document.querySelector("span#azerty") === null){
         case "readsololeveling.org":
         case "ww2.readneverland.com":
         case "ww3.readneverland.com":
-        case "ww1.readtowerofgod.com":
         case "ww1.readvinlandsaga.com":
         case "ww3.read7deadlysins.com":
         case "ww2.readhxh.com":
             readblackcloverF();
             break;
-        case "ww3.readopm.com":
         case "ww5.readopm.com":
         case "readberserk.com":
             readopmF();
             break;
-        case "kiryuu.co":
         case "kiryuu.org":
-            kiryuuF();              // sektekomik.com is the same as kiryuu.co
-            break;
-        case "sektekomik.com":
-            sektekomikF();
+            kiryuuF();
             break;
         case "mangadex.org":
             mangadexF();
             break;
-        case "rawdevart.com":
-            rawdevartF();
-            break;
         case "mangasee123.com":
         case "manga4life.com":
-            mangasee123F();         // manga4life.com is the same as mangasee123.com
+            mangasee123F();
             break;
         case "www.webtoons.com":
             webtoonsF();
             break;
-        case "www.mangasail.co":
-        case "www.mangatail.me":
-        case "www.mangasaki.com":
+        case "www.sailmg.com":
             mangasailF();
-            break;
-        case "readmanhwa.com":
-            readmanhwaF();
             break;
         case "www.mangatown.com":
             mangatownF();
             break;
-        case "loveheaven.net":  // changed to lovehug.net
-        case "kissaway.net":
-        case "klmanga.com":
+        case "klz9.com":
             loveheavenF();
-            break;
-        case "www.qiman6.com":
-            qiman6F();
-            break;
-        case "qingman5.com":
-            qingman5F();
-            break;
-        case "full-metal-alchemist.com":
-            full_metal_alchemistF();
-            break;
-        case "www.funmanga.com":
-        case "www.mngdoom.com":
-        case "www.mangainn.net":
-            funmangaF();
-            break;
-        case "mangafast.net":
-        case "comic.mangafast.net":
-            mangafastF();
-            break;
-        case "lovehug.net":
-            lovehugF();
             break;
         default:
             // alert("This should not happen");
